729026d017bf85d85a8cd78d119e00583b756acb
Enric Velasco Garrofé <evelasco@alfredsmart.com>
2021-12-27 15:34:58 +0100

    fix comments

--- App.js
+++ App.js
@@ -3,11 +3,9 @@ import {useColorScheme} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {LoginStatusProvider} from './common/contexts/loginStatusContext';
 import Navigation from './common/navigation';
-import GetNewsList from './core/domain/news/repositories/getNewsList';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  // GetNewsList().then(res => console.log('RES::::', res[0].title));
   return (
     <LoginStatusProvider>
       <SafeAreaProvider>
