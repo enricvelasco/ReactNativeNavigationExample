9db1d25f6a2f3e4f7fa0a37f4c3150eb6948dc26
Enric Velasco Garrofé <evelasco@alfredsmart.com>
2022-01-04 15:47:17 +0100

    fix height

--- common/components/ui/organisms/newsList/index.js
+++ common/components/ui/organisms/newsList/index.js
@@ -26,6 +26,7 @@ const NewsList = ({list, onSelectItem = () => {}}) => {
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    height: '100%',
     flex: 1,
     alignItems: 'center',
   },
