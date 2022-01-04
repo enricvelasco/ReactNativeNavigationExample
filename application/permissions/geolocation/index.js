8201208d261d0d2f731aba606c5971828dcee86a
Enric Velasco Garrofé <evelasco@alfredsmart.com>
2022-01-04 10:35:47 +0100

    fix geolocation updates & add iWatch comunication

--- application/permissions/geolocation/index.js
+++ application/permissions/geolocation/index.js
@@ -1,14 +1,19 @@
-import RNLocation from 'react-native-location';
+import RNLocation, { subscribeToSignificantLocationUpdates } from "react-native-location";
+
+const geolocationConfig = async () => {
+  return await RNLocation.configure({
+    distanceFilter: 5.0,
+  });
+};
 
 export const requestGeolocationPermission = async () => {
-  console.log('REQUEST_PERM:::');
   return await RNLocation.requestPermission({
     ios: 'whenInUse',
     android: {
-      detail: 'coarse',
+      detail: 'fine',
       rationale: {
-        title: 'We need to access your location',
-        message: 'We use your location to show where you are on the map',
+        title: 'Location permission',
+        message: 'We use your location to demo the library',
         buttonPositive: 'OK',
         buttonNegative: 'Cancel',
       },
@@ -16,43 +21,7 @@ export const requestGeolocationPermission = async () => {
   });
 };
 
-export const checkGeolocationPermission = async () => {
-  return await RNLocation.checkPermission({
-    ios: 'whenInUse', // or 'always'
-    android: {
-      detail: 'coarse', // or 'fine'
-    },
-  });
-};
-
-export const getCurrentPermission = async () => {
-  return await RNLocation.getCurrentPermission();
-};
-
-export const subscribeToLocationUpdates = async () => {
-  return await RNLocation.subscribeToLocationUpdates();
-};
-
-export const subscribeToPermissionsUpdates = async () => {
-  return await RNLocation.subscribeToPermissionUpdates();
-};
-
-export const getLocation = async () => {
-  const permission = await checkGeolocationPermission();
-  console.log('PERMISSIONS::::', permission);
-  if (permission) {
-    return await RNLocation.getLatestLocation({timeout: 500});
-  } else {
-    requestGeolocationPermission().then(perm => {
-      // console.log('PERM---->', perm);
-      // getLocation();
-      if (perm) {
-        getLocation();
-      } else {
-        getCurrentPermission.then(currentPermission => {
-          console.log('CURRENT_PERMISSION', currentPermission);
-        });
-      }
-    });
-  }
+export const initGeolocation = async () => {
+  await geolocationConfig();
+  return await requestGeolocationPermission();
 };
